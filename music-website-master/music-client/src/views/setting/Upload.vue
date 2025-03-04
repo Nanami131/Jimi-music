9c4eb08a7b79fad922bf3317fc7a89d4a50bdabf
Nanami131 <2920383858@qq.com>
2025-03-04 12:47:43 +0800

    修复了部分请求由于前端使用的组件默认不携带

--- music-website-master/music-client/src/views/setting/Upload.vue
+++ music-website-master/music-client/src/views/setting/Upload.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="upload">
-    <el-upload drag :action="uploadUrl()" :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+    <el-upload
+        drag
+        :action="uploadUrl()"
+        :show-file-list="false"
+        :on-success="handleAvatarSuccess"
+        :before-upload="beforeAvatarUpload"
+        :http-request="customUpload"
+    >
       <el-icon class="el-icon--upload"><upload-filled /></el-icon>
       <div class="el-upload__text">将文件拖到此处或点击上传</div>
       <template #tip>
@@ -15,11 +22,10 @@ import { defineComponent, ref, computed, getCurrentInstance } from "vue";
 import { useStore } from "vuex";
 import { UploadFilled } from "@element-plus/icons-vue";
 import { HttpManager } from "@/api";
+import axios from "axios";
 
 export default defineComponent({
-  components: {
-    UploadFilled,
-  },
+  components: { UploadFilled },
   setup() {
     const { proxy } = getCurrentInstance();
     const store = useStore();
@@ -28,31 +34,44 @@ export default defineComponent({
     const userId = computed(() => store.getters.userId);
 
     function uploadUrl() {
-      return HttpManager.uploadUrl(userId.value);
+      return HttpManager.uploadUrl(userId.value); // http://localhost:8888/user/avatar/update?id=64
     }
 
     function beforeAvatarUpload(file) {
       const ltCode = 2;
       const isLt10M = file.size / 1024 / 1024;
       const isExistFileType = uploadTypes.value.includes(file.type.replace(/image\//, ""));
-
       if (isLt10M > ltCode || isLt10M <= 0) {
         (proxy as any).$message.error(`图片大小范围是 0~${ltCode}MB!`);
       }
       if (!isExistFileType) {
         (proxy as any).$message.error(`图片只支持 ${uploadTypes.value.join("、")} 格式!`);
       }
-
       return isLt10M && isExistFileType;
     }
 
     function handleAvatarSuccess(response, file) {
-      (proxy as any).$message({
-        message: response.message,
-        type: response.type,
-      });
-
+      (proxy as any).$message({ message: response.message, type: response.type });
       if (response.success) proxy.$store.commit("setUserPic", response.data);
+    }
+
+    function customUpload(options) {
+      const formData = new FormData();
+      formData.append("file", options.file); // 确保字段名与后端一致
+
+      axios.post(options.action, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        withCredentials: true, // 确保携带 Cookie
+        onUploadProgress: options.onProgress,
+      }).then(response => {
+        console.log("Upload success:", response.data); // 调试
+        options.onSuccess(response.data);
+      }).catch(error => {
+        console.error("Upload error:", error.response || error); // 调试
+        options.onError(error);
+      });
     }
 
     return {
@@ -60,6 +79,7 @@ export default defineComponent({
       uploadUrl,
       beforeAvatarUpload,
       handleAvatarSuccess,
+      customUpload,
     };
   },
 });
