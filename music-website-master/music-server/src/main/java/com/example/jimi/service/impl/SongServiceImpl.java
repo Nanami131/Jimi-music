7dc5a0f4d08e46482fdb1c7f44b149847f4db5d2
Nanami131 <2920383858@qq.com>
2025-03-07 19:10:09 +0800

    修复歌词处理逻辑 完善缓存删除

--- music-website-master/music-server/src/main/java/com/example/jimi/service/impl/SongServiceImpl.java
+++ music-website-master/music-server/src/main/java/com/example/jimi/service/impl/SongServiceImpl.java
@@ -24,6 +24,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Date;
@@ -54,27 +55,27 @@ public class SongServiceImpl extends ServiceImpl<SongMapper, Song> implements So
     }
 
     @Override
-    public R addSong(SongRequest addSongRequest, MultipartFile lrcfile,MultipartFile mpfile){
+    public R addSong(SongRequest addSongRequest, MultipartFile lrcfile, MultipartFile mpfile) {
 
-        if(addSongRequest.getName()==null||"".equals(addSongRequest.getName())){
+        if (addSongRequest.getName() == null || "".equals(addSongRequest.getName())) {
             return R.error("必须指定歌曲名");
         }
         String introduction = addSongRequest.getIntroduction();
-        if(introduction!=null&&introduction.length()>250){
-            addSongRequest.setIntroduction(introduction.substring(0,250));
+        if (introduction != null && introduction.length() > 250) {
+            addSongRequest.setIntroduction(introduction.substring(0, 250));
         }
         Song song = new Song();
         BeanUtils.copyProperties(addSongRequest, song);
         String pic = "/img/songPic/tubiao.jpg";
         String fileName = mpfile.getOriginalFilename();
         String s = MinioUploadController.uploadFile(mpfile);
-        String storeUrlPath = "/"+bucketName+"/" + fileName;
+        String storeUrlPath = "/" + bucketName + "/" + fileName;
         song.setCreateTime(new Date());
         song.setUpdateTime(new Date());
         song.setPic(pic);
         song.setUrl(storeUrlPath);
 
-        if (lrcfile!=null&&(song.getLyric().equals("[00:00:00]暂无歌词"))){
+        if (lrcfile != null && (song.getLyric().equals("[00:00:00]暂无歌词"))) {
             byte[] fileContent = new byte[0];
             try {
                 fileContent = lrcfile.getBytes();
@@ -84,7 +85,7 @@ public class SongServiceImpl extends ServiceImpl<SongMapper, Song> implements So
                 throw new RuntimeException(e);
             }
         }
-        if (s.equals("File uploaded successfully!")&&songMapper.insert(song) > 0) {
+        if (s.equals("File uploaded successfully!") && songMapper.insert(song) > 0) {
             return R.success("上传成功", storeUrlPath);
         } else {
             return R.error("上传失败");
@@ -94,8 +95,8 @@ public class SongServiceImpl extends ServiceImpl<SongMapper, Song> implements So
     @Override
     public R updateSongMsg(SongRequest updateSongRequest) {
         String introduction = updateSongRequest.getIntroduction();
-        if(introduction!=null&&introduction.length()>250){
-            updateSongRequest.setIntroduction(introduction.substring(0,250));
+        if (introduction != null && introduction.length() > 250) {
+            updateSongRequest.setIntroduction(introduction.substring(0, 250));
         }
         Song song = new Song();
         BeanUtils.copyProperties(updateSongRequest, song);
@@ -113,7 +114,7 @@ public class SongServiceImpl extends ServiceImpl<SongMapper, Song> implements So
         String[] parts = path.split("/");
         String fileName = parts[parts.length - 1];
 
-        RemoveObjectArgs removeObjectArgs=RemoveObjectArgs.builder()
+        RemoveObjectArgs removeObjectArgs = RemoveObjectArgs.builder()
                 .bucket(bucketName)
                 .object(fileName)
                 .build();
@@ -140,11 +141,11 @@ public class SongServiceImpl extends ServiceImpl<SongMapper, Song> implements So
         } catch (XmlParserException e) {
             throw new RuntimeException(e);
         }
-        String storeUrlPath = "/"+bucketName+"/" + fileName;
+        String storeUrlPath = "/" + bucketName + "/" + fileName;
         song.setId(id);
         song.setUrl(storeUrlPath);
         song.setName(fileName);
-        if (s.equals("File uploaded successfully!")&&songMapper.updateById(song) > 0) {
+        if (s.equals("File uploaded successfully!") && songMapper.updateById(song) > 0) {
             return R.success("更新成功", storeUrlPath);
         } else {
             return R.error("更新失败");
@@ -153,13 +154,20 @@ public class SongServiceImpl extends ServiceImpl<SongMapper, Song> implements So
 
     @Override
     public R updateSongPic(MultipartFile urlFile, int id) {
-        String originalFilename =  urlFile.getOriginalFilename();
-        String fileName = FileNameUtils.defineNamePath(originalFilename,"/singer/song/",id);
+        String originalFilename = urlFile.getOriginalFilename();
+        String fileName = FileNameUtils.defineNamePath(originalFilename, "/singer/song/", id);
         Song song = new Song();
         song.setId(id);
-        song.setPic("/user01"+fileName);
-        String s = MinioUploadController.uploadImgFile(urlFile,fileName);
+        song.setPic("/user01" + fileName);
+        String s = MinioUploadController.uploadImgFile(urlFile, fileName);
         if (s.equals("File uploaded successfully!") && songMapper.updateById(song) > 0) {
+            QueryWrapper<Song> queryWrapper = new QueryWrapper<>();
+            queryWrapper
+                    .select("singer_id") // 只查询 singer_id 字段
+                    .eq("id", id);       // 条件：id 等于传入的 id
+            //还有歌手部分歌曲的图片缓存也要删除
+            stringRedisTemplate.delete("song:singerId:" + songMapper.selectOne(queryWrapper).getSingerId());
+            stringRedisTemplate.delete("song:id:" + id);
             return R.success("上传成功", fileName);
         } else {
             return R.error("上传失败");
@@ -173,7 +181,7 @@ public class SongServiceImpl extends ServiceImpl<SongMapper, Song> implements So
         String[] parts = path.split("/");
         String fileName = parts[parts.length - 1];
         System.out.println(fileName);
-        RemoveObjectArgs removeObjectArgs=RemoveObjectArgs.builder()
+        RemoveObjectArgs removeObjectArgs = RemoveObjectArgs.builder()
                 .bucket(bucketName)
                 .object(fileName)
                 .build();
@@ -199,6 +207,7 @@ public class SongServiceImpl extends ServiceImpl<SongMapper, Song> implements So
             } catch (XmlParserException e) {
                 throw new RuntimeException(e);
             }
+            stringRedisTemplate.delete("song:singerId:" + id);
             return R.success("删除成功");
         } else {
             return R.error("删除失败");
@@ -207,46 +216,48 @@ public class SongServiceImpl extends ServiceImpl<SongMapper, Song> implements So
 
     @Override
     public R songOfSingerId(Integer singerId) {
-        String key =stringRedisTemplate.opsForValue().get("song:singerId:"+singerId);
-        if(key!=null){
+        String key = stringRedisTemplate.opsForValue().get("song:singerId:" + singerId);
+        if (key != null) {
             try {
-                List<Song> songs=objectMapper.readValue(key, new TypeReference<List<Song>>(){});
-                return R.success("走了缓存",songs);
+                List<Song> songs = objectMapper.readValue(key, new TypeReference<List<Song>>() {
+                });
+                return R.success("走了缓存", songs);
             } catch (JsonProcessingException e) {
                 return R.error(e.getMessage());
             }
         }
         QueryWrapper<Song> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("singer_id",singerId);
+        queryWrapper.eq("singer_id", singerId);
         List<Song> songs = songMapper.selectList(queryWrapper);
         try {
-            stringRedisTemplate.opsForValue().set("song:singerId:"+singerId,
-                    objectMapper.writeValueAsString(songs),5, TimeUnit.MINUTES);
+            stringRedisTemplate.opsForValue().set("song:singerId:" + singerId,
+                    objectMapper.writeValueAsString(songs), 5, TimeUnit.MINUTES);
         } catch (JsonProcessingException e) {
-            log.error("缓存建立失败: {}",  e);
+            log.error("缓存建立失败: {}", e);
         }
         return R.success("没有走缓存", songs);
     }
 
     @Override
     public R songOfId(Integer id) {
-        String key =stringRedisTemplate.opsForValue().get("song:id:"+id);
-        if(key!=null){
+        String key = stringRedisTemplate.opsForValue().get("song:id:" + id);
+        if (key != null) {
             try {
-                List<Song> songs=objectMapper.readValue(key, new TypeReference<List<Song>>(){});
-                return R.success("走了缓存",songs);
+                List<Song> songs = objectMapper.readValue(key, new TypeReference<List<Song>>() {
+                });
+                return R.success("走了缓存", songs);
             } catch (JsonProcessingException e) {
                 return R.error(e.getMessage());
             }
         }
         QueryWrapper<Song> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("id",id);
+        queryWrapper.eq("id", id);
         List<Song> songs = songMapper.selectList(queryWrapper);
         try {
-            stringRedisTemplate.opsForValue().set("song:id:"+id,
-                        objectMapper.writeValueAsString(songs),5, TimeUnit.MINUTES);
+            stringRedisTemplate.opsForValue().set("song:id:" + id,
+                    objectMapper.writeValueAsString(songs), 5, TimeUnit.MINUTES);
         } catch (JsonProcessingException e) {
-            log.error("缓存建立失败: {}",  e);
+            log.error("缓存建立失败: {}", e);
         }
         return R.success("没有走缓存", songs);
     }
@@ -254,9 +265,9 @@ public class SongServiceImpl extends ServiceImpl<SongMapper, Song> implements So
     @Override
     public R songOfSingerName(String name) {
         QueryWrapper<Song> queryWrapper = new QueryWrapper<>();
-        queryWrapper.like("name",name);
+        queryWrapper.like("name", name);
         List<Song> songs = songMapper.selectList(queryWrapper);
-        if (songs.isEmpty()){
+        if (songs.isEmpty()) {
             return R.error("添加失败，没有找到该歌,无法加入该歌单");
         }
 
@@ -266,17 +277,23 @@ public class SongServiceImpl extends ServiceImpl<SongMapper, Song> implements So
     @Override
     public R updateSongLrc(MultipartFile lrcFile, int id) {
         Song song = songMapper.selectById(id);
-        if (lrcFile!=null&&!(song.getLyric().equals("[00:00:00]暂无歌词"))){
-            byte[] fileContent = new byte[0];
-            try {
-                fileContent = lrcFile.getBytes();
-                String content = new String(fileContent, "GB2312");
-                song.setLyric(content);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        byte[] fileContent = new byte[0];
+        try {
+            fileContent = lrcFile.getBytes();
+            String content = new String(fileContent, StandardCharsets.UTF_8);
+            song.setLyric(content);
+            System.out.println("1234"+content);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         if (songMapper.updateById(song) > 0) {
+            QueryWrapper<Song> queryWrapper = new QueryWrapper<>();
+            queryWrapper
+                    .select("singer_id") // 只查询 singer_id 字段
+                    .eq("id", id);       // 条件：id 等于传入的 id
+            //还有歌手部分歌曲的图片缓存也要删除
+            stringRedisTemplate.delete("song:singerId:" + songMapper.selectOne(queryWrapper).getSingerId());
+            stringRedisTemplate.delete("song:id:" + id);
             return R.success("更新成功");
         } else {
             return R.error("更新失败");
